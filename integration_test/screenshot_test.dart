89c526fa426c36aba1f2ac51651337cc4c67a155
wipe <wilhelm.perdahl@gmail.com>
2023-08-04 13:54:37 +0200

    chore: Remove prints

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -7,7 +7,6 @@ import 'package:integration_test/integration_test.dart';
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  print('DEVICE_NAME: ${const String.fromEnvironment('DEVICE_NAME')}');
   group('Screenshot tests', () {
     setUpAll(() {
       return Future(() async {
@@ -15,16 +14,12 @@ void main() {
         if (Platform.isAndroid) {
           await binding.convertFlutterSurfaceToImage();
         }
-        print("set up complete");
       });
     });
     testWidgets('render home and navigate', (tester) async {
-      print("starting main");
       app.main();
-      print("main started");
       await tester.pumpAndSettle();
       await binding.takeScreenshot('0-home');
-      print("screenshot taken");
     });
   });
 }
--- test_driver/screenshot_test.dart
+++ test_driver/screenshot_test.dart
@@ -3,15 +3,11 @@ import 'dart:io';
 import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
-  String deviceName = const String.fromEnvironment('DEVICE_NAME');
-  String devicePrefix = deviceName.isEmpty ? '' : '${deviceName}_';
-  print(devicePrefix);
   await integrationDriver(
     onScreenshot: (String screenshotName, List<int> screenshotBytes,
         [Map<String, Object?>? json]) async {
       final File image =
-          await File('screenshots/$devicePrefix$screenshotName.png')
-              .create(recursive: true);
+          await File('screenshots/$screenshotName.png').create(recursive: true);
       image.writeAsBytesSync(screenshotBytes);
       return true;
     },
